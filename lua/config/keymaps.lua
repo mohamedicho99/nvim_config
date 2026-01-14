81ee961a709935ffc64867a737ba98c2a1611e00
Mohamed El Moudden <mohel-mo@c4r10p10.1337.ma>
2026-01-14 04:46:14 +0100

    keymaps to move lines up and down in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,3 @@
--- Keymaps
-
 -- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -9,3 +7,6 @@ vim.keymap.set('n', 'j', 'gj', { desc = 'Down', noremap = true})
 vim.keymap.set('n', 'k', 'gk', { desc = 'Up', noremap = true})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- Move selected blocks up and down in Visual Mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
